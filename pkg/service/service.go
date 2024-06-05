780508e6f1d098dc819fab9776e91f5210700f5b
yutahasegawa178 <yuta.eru178@gmail.com>
2024-06-05 21:41:07 +0900

    add: task service

---
+++ pkg/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"github.com/yhsgw178/todoapp/pkg/models"
+	"github.com/yhsgw178/todoapp/pkg/repository"
+)
+
+type TaskService interface {
+	CreateTask(task *models.Task) error
+	// GetTaskByID(id int) (*models.Task, error)
+	// GetAllTasks() ([]*models.Task, error)
+	// UpdateTask(task *models.Task) error
+	// DeleteTask(id int) error
+}
+
+type TaskServiceImpl struct {
+	repo repository.TaskRepository
+}
+
+func NewTaskService(repo repository.TaskRepository) TaskService {
+	return &TaskServiceImpl{
+		repo: repo,
+	}
+}
+
+func (t *TaskServiceImpl) CreateTask(task *models.Task) error {
+	return t.repo.CreateTask(task)
+}
