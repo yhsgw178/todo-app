d392005bdb08f2a62ea44fc6ad9e1009e3879afb
yutahasegawa178 <yuta.eru178@gmail.com>
2024-06-05 21:41:00 +0900

    add: task model

---
+++ pkg/models/task.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"time"
+)
+
+// TaskStatus represents the status of a task.
+type TaskStatus int
+
+const (
+	TODO TaskStatus = iota
+	Processing
+	Done
+	Pending
+)
+
+// Task represents a task in the to-do list.
+type Task struct {
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description,omitempty"`
+	IsCompleted bool       `json:"is_completed"`
+	DueDate     time.Time  `json:"due_date"`
+	TaskStatus  TaskStatus `json:"task_status"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
