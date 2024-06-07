f5ef48daa3f05ea5200440e7e94f80258c141a37
yutahasegawa178 <yuta.eru178@gmail.com>
2024-06-08 01:14:03 +0900

    db: add get task

--- pkg/repository/postgres_task.go
+++ pkg/repository/postgres_task.go
@@ -27,3 +27,23 @@ func (r *PostgresTaskRepository) CreateTask(task *models.Task) error {
 	}
 	return nil
 }
+
+func (r *PostgresTaskRepository) GetTaskByID(id int) (*models.Task, error) {
+	query := `SELECT id, title, description, is_completed, due_date, task_status, created_at, updated_at FROM tasks WHERE id = $1;`
+
+	row := r.DB.QueryRow(query, id)
+	return scanTask(row)
+}
+
+func scanTask(rows *sql.Row) (*models.Task, error) {
+	task := &models.Task{}
+	err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.IsCompleted, &task.DueDate, &task.TaskStatus, &task.CreatedAt, &task.UpdatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return task, nil
+}
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -6,7 +6,7 @@ import (
 
 type TaskRepository interface {
 	CreateTask(task *models.Task) error
-	// GetTaskByID(id int) (*models.Task, error)
+	GetTaskByID(id int) (*models.Task, error)
 	// GetAllTasks() ([]*models.Task, error)
 	// UpdateTask(task *models.Task) error
 	// DeleteTask(id int) error
